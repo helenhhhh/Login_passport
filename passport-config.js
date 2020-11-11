d8d5e747b63675796b6324cc5f971b75318c7d4b
helen <yy2jwy@gmial.com>
2020-11-11 00:04:05 -0800

    login

---
+++ passport-config.js
@@ -0,0 +1,25 @@
+const { authenticate } = require('passport')
+const bcrypt = require('bcrypt'); 
+
+const localStrategy = require('passport-local').Strategy;
+ function initialize(passport,getUserByEmail,getUserById ){
+  const authenticateUser = async (email, password, done)=>{
+    const user= getUserByEmail(email);
+    if(user == null){
+      return done(null,false,{message:'no user with that email'}) //done 1 err msessage, 2 user 3 message
+    }
+    try{
+        if(await bcrypt.compare(password,user.password)){
+          return done(null,user)
+        }else{
+          return done(null,false,{message:'password incorrect'})
+        }
+    }catch(e){
+      return done(e);
+    }
+  }
+  passport.use(new localStrategy({usernameField: 'email'},authenticateUser));
+  passport.serializeUser((user,done)=>done(null, user.id));
+  passport.deserializeUser((id,done)=>done(null, getUserById(id)));
+}
+module.exports = initialize; 
